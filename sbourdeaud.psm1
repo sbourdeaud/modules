d974f271a22919a6de2b360ba5df4c6da031c983
sbourdeaud <stephane.bourdeaud@nutanix.com>
2017-05-17 13:30:16 +0200

    Adding Write-LogOutput function and manifest.

---
+++ sbourdeaud.psm1
@@ -0,0 +1,70 @@
+#region functions
+
+function Write-LogOutput
+{
+<#
+.SYNOPSIS
+Outputs color coded messages to the screen and/or log file based on the category.
+
+.DESCRIPTION
+This function is used to produce screen and log output which is categorized, time stamped and color coded.
+
+.PARAMETER Category
+This the category of message being outputed. If you want color coding, use either "INFO", "WARNING", "ERROR" or "SUM".
+
+.PARAMETER Message
+This is the actual message you want to display.
+
+.PARAMETER LogFile
+If you want to log output to a file as well, use logfile to pass the log file full path name.
+
+.NOTES
+Author: Stephane Bourdeaud (sbourdeaud@nutanix.com)
+
+.EXAMPLE
+PS> Write-LogOutput -category "ERROR" -message "You must be kidding!"
+
+Displays an error message.
+
+.LINK
+https://github.com/sbourdeaud
+#>
+    [CmdletBinding(DefaultParameterSetName = 'None')] #make this function advanced
+
+	param
+	(
+		[Parameter(Mandatory)]
+        [ValidateSet('INFO','WARNING','ERROR','SUM')]
+        [string]
+        $Category,
+
+        [string]
+		$Message,
+
+        [string]
+        $LogFile
+	)
+
+    process
+    {
+        $Date = get-date #getting the date so we can timestamp the output entry
+	    $FgColor = "Gray" #resetting the foreground/text color
+	    switch ($Category) #we'll change the text color depending on the selected category
+	    {
+		    "INFO" {$FgColor = "Green"}
+		    "WARNING" {$FgColor = "Yellow"}
+		    "ERROR" {$FgColor = "Red"}
+		    "SUM" {$FgColor = "Magenta"}
+	    }
+
+	    Write-Host -ForegroundColor $FgColor "$Date [$category] $Message" #write the entry on the screen
+	    if ($LogFile) #add the entry to the log file if -LogFile has been specified
+        {
+            Add-Content -Path $LogFile -Value "$Date [$Category] $Message"
+            Write-Verbose -Message "Wrote entry to log file $LogFile" #specifying that we have written to the log file if -verbose has been specified
+        }
+    }
+
+}#end function Write-LogOutput
+
+#endregion
